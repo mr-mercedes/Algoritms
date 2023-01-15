db898b861df04309f4c649606d21568276a813d9
Ekyd <Ekyd777@gmail.com>
2023-01-15 20:42:11 +0300

    add task 1, 2

---
+++ src/main/java/org/example/Task2.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Scanner;
+
+/*
+Найти n-ое число Фибоначчи за O(n).
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        new Task2().run();
+    }
+
+    void run() {
+        try (Scanner in = new Scanner(System.in)) {
+            solve(in);
+        }
+    }
+
+    void solve(Scanner in) {
+        System.out.print("Enter number of febo: ");
+        String number = in.next();
+        int answer = feboLine(Integer.parseInt(number));
+        System.out.println(number + "th of febonachi is " + answer);
+        System.out.println("Сложность алгоритма O(n)");
+    }
+
+    private int feboLine(int number) {
+        int firstNumber = 1;
+        int secondNumber = 1;
+        int swapNumber;
+        int counter = number;
+        while (--counter > 0){
+            swapNumber = secondNumber;
+            secondNumber += firstNumber;
+            firstNumber = swapNumber;
+        }
+        return secondNumber;
+    }
+}
