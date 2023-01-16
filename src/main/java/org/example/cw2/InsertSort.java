6cbb7a98d6f98ad4312cba8af5aedc9930f96be6
Ekyd <Ekyd777@gmail.com>
2023-01-16 12:15:59 +0300

    add BinarySearch

--- src/main/java/org/example/cw2/InsertSort.java
+++ src/main/java/org/example/cw2/InsertSort.java
@@ -9,7 +9,7 @@ public class InsertSort {
         System.out.println(Arrays.toString(array));
     }
 
-    private static void insertSort(int[] array) {
+    public static void insertSort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] > array[j]){
