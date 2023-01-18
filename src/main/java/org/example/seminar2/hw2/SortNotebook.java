1c8d3ae4813a6d5ca56cc2951772401359fe2216
Ekyd <Ekyd777@gmail.com>
2023-01-18 22:33:52 +0300

    optimization import

--- src/main/java/org/example/seminar2/hw2/SortNotebook.java
+++ src/main/java/org/example/seminar2/hw2/SortNotebook.java
@@ -1,9 +1,6 @@
 package org.example.seminar2.hw2;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
 
 public class SortNotebook{
     protected static ArrayList<Notebook> priceSortNotebook(ArrayList<Notebook> array){
