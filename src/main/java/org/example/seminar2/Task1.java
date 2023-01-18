2592128c2e13ba67866a1dc316ddea33ac1fb6f5
Ekyd <Ekyd777@gmail.com>
2023-01-18 23:02:46 +0300

    merge to seminar2

--- src/main/java/org/example/seminar2/Task1.java
+++ src/main/java/org/example/seminar2/Task1.java
@@ -1,4 +1,20 @@
-package main.java.org.example.seminar2;
+package org.example.seminar2;
 
 public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    public static void insertionSort(int[] sortArr) {
+        int j;
+        //сортировку начинаем со второго элемента, т.к. считается, что первый элемент уже отсортирован
+        for (int i = 1; i < sortArr.length; i++) {
+            //сохраняем ссылку на индекс предыдущего элемента
+            int swap = sortArr[i];
+            for (j = i; j > 0 && swap < sortArr[j - 1]; j--) {
+                //элементы отсортированного сегмента перемещаем вперёд, если они больше элемента для вставки
+                sortArr[j] = sortArr[j - 1];
+            }
+            sortArr[j] = swap;
+        }
+    }
 }
